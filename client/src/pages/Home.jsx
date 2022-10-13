622586a2bfb475ef7d5747eaecd60b7e35404183
jiajia <sijiay@seas.upenn.edu>
2022-10-12 20:10:25 -0400

    incorporate Carousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Topbar from"../components/Topbar"
+import Carousel from "../components/Carousel";
 export default function Home() {
   return (
     <div>
       <Topbar/>
+      <Carousel/>
     </div>
   );
 }
