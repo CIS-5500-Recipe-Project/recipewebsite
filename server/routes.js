63a21ed08d6e83e68c842d94dde3ff2c350c4f5b
Jimeng927 <jimeng.jiang@gmail.com>
2022-11-13 17:13:59 -0500

    add query templates for creating APIs

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,16 @@
-// const config = require('./config.json')
-// const mysql = require('mysql');
-// const e = require('express');
+const config = require('./config.json')
+const mysql = require('mysql');
+const e = require('express');
 
 // // TODO: fill in your connection details here
-// const connection = mysql.createConnection({
-//     host: config.rds_host,
-//     user: config.rds_user,
-//     password: config.rds_password,
-//     port: config.rds_port,
-//     database: config.rds_db
-// });
-// connection.connect();
+const connection = mysql.createConnection({
+    host: config.rds_host,
+    user: config.rds_user,
+    password: config.rds_password,
+    port: config.rds_port,
+    database: config.rds_db
+});
+connection.connect();
 
 
 // ********************************************
@@ -20,8 +20,48 @@
 // Route 1 Individual Recipe
 // TODO: fill in query to grab info regarding specific recipe 
 async function recipes(req, res) {
-    // a GET request to /recipes
-    res.send(`this is recipe page`)
+
+    var inputDescription = req.query.attribute ? req.query.attribute : ""
+    var queryDescription = `SELECT RecipeId, Name, AggregatedRating, ReviewCount, DatePublished
+    FROM recipes
+    WHERE Description LIKE '%${inputDescription}%'
+    ORDER BY ReviewCount DESC, AggregatedRating DESC, DatePublished DESC
+    LIMIT 50`
+
+    var queryKeyword = `SELECT RecipeId, Name, AggregatedRating, ReviewCount, DatePublished
+    FROM recipes
+    WHERE Keywords LIKE '%${inputDescription}%'
+    ORDER BY ReviewCount DESC, AggregatedRating DESC, DatePublished DESC
+    LIMIT 50`
+
+
+    // http://localhost:8080/recipes/description?decription=summer
+    // http://localhost:8080/recipes/description?decription=vegan
+    if (req.params.choice === 'description') {
+
+        connection.query(queryDescription, function(err, results, fields) {
+            if (err) console.log(err);
+            else {
+                console.log(results);
+                res.json(results)
+            }
+        })
+    } else if (req.params.choice === 'keyword') { // http://localhost:8080/recipes/keyword?keyword=kid
+
+        connection.query(queryKeyword, function(err, results, fields) {
+            if (err) console.log(err);
+            else {
+                console.log(results);
+                res.json(results)
+            }
+        })
+
+    }
+
+
+    
+
+    
 }
 
 // Route 2 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require("express")
-const app = express()
+const mysql      = require('mysql');
+var cors = require('cors')
 const port = process.env.PORT || 3000
 const routes = require("./routes")
 
-app.listen(port)
+const config = require('./config.json')
+const app = express();
+
+//app.listen(port)
+
+// whitelist localhost 3000
+app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 
 //main app page
 app.get('/', (req, res) => { res.send("Hungry Time") })
 //recipe page
-app.get('/recipes', routes.recipes)
+app.get('/recipes:choice', routes.recipes)
 //pageTwo
 app.get('/pageTwo', routes.pageTwo)
 
+
+app.listen(config.server_port, () => {
+    console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
+});
+
+module.exports = app;
