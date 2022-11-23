389131e9f4de01b98f0f661403eebcb519208c89
jiajia <sijiay@seas.upenn.edu>
2022-11-22 21:54:36 -0500

    add similar Recipe Part

---
+++ client/src/components/Recipe/SimilarRecipe.js
@@ -0,0 +1,172 @@
+import "../css/main.css";
+import "../css/css.css";
+import "../css/css1.css";
+import "uikit/dist/js/uikit.js";
+
+export default function SmilarRecipe({ recipe }) {
+  return (
+    <div class="uk-section uk-section-default">
+      <div class="uk-container uk-container-small">
+        <div class="uk-grid-large" data-uk-grid="">
+          <div class="uk-width-expand@m">
+            <div class="uk-article">
+              <h3>Similar Recipes</h3>
+              <div
+                class="uk-child-width-1-2 uk-child-width-1-4@s"
+                data-uk-grid=""
+              >
+                <div class="uk-card">
+                  <div class="uk-card-media-top uk-inline uk-light">
+                    <img
+                      class="uk-border-rounded-medium"
+                      src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+                      alt="Course Title"
+                    />
+                    <div class="uk-position-cover uk-card-overlay uk-border-rounded-medium"></div>
+                    <div class="uk-position-xsmall uk-position-top-right">
+                      <a
+                        href="#"
+                        class="uk-icon-button uk-like uk-position-z-index uk-position-relative"
+                        data-uk-icon="heart"
+                      ></a>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 class="uk-card-title uk-text-500 uk-margin-small-bottom uk-margin-top">
+                      Chef John's Turkey Sloppy Joes
+                    </h3>
+                    <div class="uk-text-xsmall uk-text-muted" data-uk-grid="">
+                      <div class="uk-width-auto uk-flex uk-flex-middle">
+                        <span
+                          class="uk-rating-filled"
+                          data-uk-icon="icon: star; ratio: 0.7"
+                        ></span>
+                        <span class="uk-margin-xsmall-left">5.0</span>
+                        <span>(73)</span>
+                      </div>
+                      <div class="uk-width-expand uk-text-right">
+                        by John Keller
+                      </div>
+                    </div>
+                  </div>
+                  <a href="recipe.html" class="uk-position-cover"></a>
+                </div>
+
+                <div class="uk-card">
+                  <div class="uk-card-media-top uk-inline uk-light">
+                    <img
+                      class="uk-border-rounded-medium"
+                      src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+                      alt="Course Title"
+                    />
+                    <div class="uk-position-cover uk-card-overlay uk-border-rounded-medium"></div>
+                    <div class="uk-position-xsmall uk-position-top-right">
+                      <a
+                        href="#"
+                        class="uk-icon-button uk-like uk-position-z-index uk-position-relative"
+                        data-uk-icon="heart"
+                      ></a>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 class="uk-card-title uk-text-500 uk-margin-small-bottom uk-margin-top">
+                      Chef John's Turkey Sloppy Joes
+                    </h3>
+                    <div class="uk-text-xsmall uk-text-muted" data-uk-grid="">
+                      <div class="uk-width-auto uk-flex uk-flex-middle">
+                        <span
+                          class="uk-rating-filled"
+                          data-uk-icon="icon: star; ratio: 0.7"
+                        ></span>
+                        <span class="uk-margin-xsmall-left">5.0</span>
+                        <span>(73)</span>
+                      </div>
+                      <div class="uk-width-expand uk-text-right">
+                        by John Keller
+                      </div>
+                    </div>
+                  </div>
+                  <a href="recipe.html" class="uk-position-cover"></a>
+                </div>
+
+                <div class="uk-card">
+                  <div class="uk-card-media-top uk-inline uk-light">
+                    <img
+                      class="uk-border-rounded-medium"
+                      src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+                      alt="Course Title"
+                    />
+                    <div class="uk-position-cover uk-card-overlay uk-border-rounded-medium"></div>
+                    <div class="uk-position-xsmall uk-position-top-right">
+                      <a
+                        href="#"
+                        class="uk-icon-button uk-like uk-position-z-index uk-position-relative"
+                        data-uk-icon="heart"
+                      ></a>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 class="uk-card-title uk-text-500 uk-margin-small-bottom uk-margin-top">
+                      Chef John's Turkey Sloppy Joes
+                    </h3>
+                    <div class="uk-text-xsmall uk-text-muted" data-uk-grid="">
+                      <div class="uk-width-auto uk-flex uk-flex-middle">
+                        <span
+                          class="uk-rating-filled"
+                          data-uk-icon="icon: star; ratio: 0.7"
+                        ></span>
+                        <span class="uk-margin-xsmall-left">5.0</span>
+                        <span>(73)</span>
+                      </div>
+                      <div class="uk-width-expand uk-text-right">
+                        by John Keller
+                      </div>
+                    </div>
+                  </div>
+                  <a href="recipe.html" class="uk-position-cover"></a>
+                </div>
+
+                <div class="uk-card">
+                  <div class="uk-card-media-top uk-inline uk-light">
+                    <img
+                      class="uk-border-rounded-medium"
+                      src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+                      alt="Course Title"
+                    />
+                    <div class="uk-position-cover uk-card-overlay uk-border-rounded-medium"></div>
+                    <div class="uk-position-xsmall uk-position-top-right">
+                      <a
+                        href="#"
+                        class="uk-icon-button uk-like uk-position-z-index uk-position-relative"
+                        data-uk-icon="heart"
+                      ></a>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 class="uk-card-title uk-text-500 uk-margin-small-bottom uk-margin-top">
+                      Chef John's Turkey Sloppy Joes
+                    </h3>
+                    <div class="uk-text-xsmall uk-text-muted" data-uk-grid="">
+                      <div class="uk-width-auto uk-flex uk-flex-middle">
+                        <span
+                          class="uk-rating-filled"
+                          data-uk-icon="icon: star; ratio: 0.7"
+                        ></span>
+                        <span class="uk-margin-xsmall-left">5.0</span>
+                        <span>(73)</span>
+                      </div>
+                      <div class="uk-width-expand uk-text-right">
+                        by John Keller
+                      </div>
+                    </div>
+                  </div>
+                  <a href="recipe.html" class="uk-position-cover"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
